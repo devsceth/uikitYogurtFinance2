3c70f6cf54d4e11a089e241d2293ae5aa2c98fa6
ColaFactory <colafactory.dev@gmail.com>
2023-07-17 13:37:07 +0200

    update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3502,7 +3502,7 @@ var templateObject_1$9;
 var PriceLink = styled.a(templateObject_1$8 || (templateObject_1$8 = __makeTemplateObject(["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"], ["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"])));
 var CakePrice = function (_a) {
     var cakePriceUsd = _a.cakePriceUsd;
-    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://coinmarketcap.com/currencies/cub-finance/", target: "_blank" },
+    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://dexscreener.com/pulsechain/0x7978de6276f5f7397e730c143f194bcb66c03cc7", target: "_blank" },
         React.createElement(Icon$W, { width: "24px", mr: "8px" }),
         React.createElement(Text, { color: "textSubtle", bold: true }, "$" + cakePriceUsd.toFixed(3)))) : (React.createElement(Skeleton, { width: 80, height: 24 }));
 };
